08cd44eb1c8cb806d384f758eabbdc1e4b9b6399
Tomas Vicar <40293035+tomasvicar@users.noreply.github.com>
2022-03-10 18:08:39 +0100

    Update test_hdf5.py

--- test_hdf5.py
+++ test_hdf5.py
@@ -105,8 +105,58 @@ with h5.File("myfile3.h5", "r") as f:
 
 
 
+# from tifffile import imread
+# import numpy as np
+# import h5py
+
+# import time
+
+# img_filename = r"D:\foky_final_cleaning\DeepFoci\data_zenodo\part1\nucleus_segmentation\train\data_041.tif"
+
+# mask_filename = r"D:\foky_final_cleaning\DeepFoci\data_zenodo\part1\nucleus_segmentation\train\mask_041.tif"
+
+
+# img = imread(img_filename)
+# mask = imread(mask_filename)>0
+
+# img = np.moveaxis(img,0,2)
+
+
+# with h5py.File("mytestfile.hdf5", "w") as f:
+    
+#     start = time.time()
+    
+    
+#     ## 2.2s 198MB; read 0.25s
+#     # dst_mask = f.create_dataset('mask/041', data=mask, chunks=(128,128,48), compression="gzip", compression_opts=2)
+    
+#     # dst_img = f.create_dataset('img/041', data=img,chunks=(128,128,48,3), compression="gzip", compression_opts=2)
+
+    
 
 
 
 
+#     ## 8.9s 368MB; read 0.31s
+#     dst_mask = f.create_dataset('mask/041', data=mask, chunks=(128,128,48), compression="lzf")
+    
+#     dst_img = f.create_dataset('img/041', data=img,chunks=(128,128,48,3), compression="lzf")
+
+
+
+#     end = time.time()
+#     print(end - start)
+    
+    
+    
+# start = time.time()
+# with h5py.File("mytestfile.hdf5", "r") as f:    
+    
+#     mask = f['mask/041'][...]
+#     img = f['mask']['041'][...]
+    
+    
+# end = time.time()
+# print(end - start)
+    
 
