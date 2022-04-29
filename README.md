2b25351241231dbaef6078cf9657c6d9785b426b
tomasvicar <tomasvicar@gmail.com>
2022-04-29 16:36:58 +0200

    a

--- README.md
+++
@@ -1,2 +0,0 @@
-# Dataformats_matlab_python
-Examles how to read various dataformats - json,, 3d tiff.
--- bfsave_volume_XYCZT.m
+++
@@ -1,8 +0,0 @@
-function bfsave_volume_XYCZT(fname,img)
-
-% it can fail if file exists
-if isfile(fname)
-    delete(fname)
-end
-bfsave(img, fname, 'dimensionOrder', 'XYCZT', 'Compression', 'LZW')
-
